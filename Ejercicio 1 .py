9c74a3ae4a9be18f82ea28f565b5de86f2a7aef5
jgonzalezt3-boop <jgonzalezt3@unemi.edu.ec>
2026-08-27 22:58:32 -0500

    Add files via upload

---
+++ Ejercicio 1 .py
@@ -0,0 +1,34 @@
+#Saludo personalizado
+
+#Entrada
+#nombre
+#Proceso
+#Concatenar el saludo con el nombre
+#Salida
+#Saludo personalizado
+
+#Bosquejo
+#nombre = "Jenniffer"
+#Salida = Hola Jenniffer. Bienvenida al curso.
+
+Nombre = input("¿Como te llamas? ")
+print (f"Hola {Nombre}. Bienvenida al curso.")
+
+#Saludo personalizado más edad
+
+#Entrada
+#nombre
+#edad
+#Proceso
+#Concatenar el saludo con el nombre y la edad
+#Salida
+#Saludo personalizado
+
+#Bosquejo
+#nombre = "Jenniffer"
+#edad = 25
+#Salida = Hola me llamo Jennifer, tengo 25 años.
+
+Nombre = input("¿Como te llamas? ")
+Edad = int(input("¿Cuantos años tienes? "))
+print (f"Hola me llamo {Nombre}, tengo {Edad} años.")
